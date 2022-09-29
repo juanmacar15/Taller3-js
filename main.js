d91092d96ddad9e36945a300fd9ca6580c77ac71
juanmacar15 <jdmadrid19@misena.edu.co>
2022-09-29 10:17:09 -0500

    ejercicio 4

--- main.js
+++ main.js
@@ -3,22 +3,15 @@ addEventListener("DOMContentLoaded", (e) => {
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        let est = document.getElementById("num1").value;
-        const canNotas = 3
-        let cont = 1;
-
-        while(cont <= est){
-            let nombre = prompt('estudiante #'+cont+'\ningrese su nombre:');
-            let apellido = prompt('ingrese su apellido: ');
-            let n1 = parseFloat(prompt('ingrese la nota 1: '));
-            let n2 = parseFloat(prompt('ingrese la nota 2: '));
-            let n3 = parseFloat(prompt('ingrese la nota 3: '));
-
-            let prom = (n1 + n2 + n3) / canNotas;
-
-            alert('el promedio del estudiante '+nombre+' '+apellido+ ' es: \nnota:'+prom);
-            cont ++;
+        // let est = document.getElementById("num1").value;
+        
+        let damas=1, caballeros=7, personas=0;
+        while(personas<42) {
+            damas++;
+            caballeros++;
+            personas=damas+caballeros;
         }
+        document.getElementById('res1').innerHTML='habian '+damas+ ' damas';
 
     })
 })
