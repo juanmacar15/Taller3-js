f4911de6eeb7ac2191d7e91e78e4a9576c404e2e
juanmacar15 <jdmadrid19@misena.edu.co>
2022-09-29 10:41:50 -0500

    ejercicio 6

--- main.js
+++ main.js
@@ -3,25 +3,17 @@ addEventListener("DOMContentLoaded", (e) => {
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        let cantEstudiantes = document.getElementById("num1").value;
-        let cont = 1;
-        let nivelacion = 0;
-        while(cont <= cantEstudiantes) {
-            let nota1 = parseFloat(prompt('alumno #'+cont+'\ningrese la nota 1'))
-            let nota2 = parseFloat(prompt("ingrese la nota 2: "));
-            let nota3 = parseFloat(prompt("ingrese la nota 3: "));
-            let nota4 = parseFloat(prompt("ingrese la nota 4: "));
-            let nota5 =parseFloat(prompt("ingrese la nota 5: "));
+        let avion = 20;
+        let proyect = 10;
+        let distancia = 0;
 
-            let prom = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
-            cont++;
-            alert('su promedio es de: '+prom)
-
-            if(prom < 3){
-                nivelacion ++;
-            }
+        while(distancia < 1000){
+            avion = avion * 2;
+            proyect = proyect * 2;
+            distancia = avion + proyect;
         }
-        document.getElementById('res1').innerHTML= nivelacion;
+        document.getElementById('res1').innerHTML= distancia +' m';
+
 
     })
 })
