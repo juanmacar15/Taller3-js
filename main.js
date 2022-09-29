6f4dc8468d1bd9c937b1a183a24deab72db56c8b
juanmacar15 <jdmadrid19@misena.edu.co>
2022-09-29 10:56:38 -0500

    ejercicio 7

--- main.js
+++ main.js
@@ -3,16 +3,24 @@ addEventListener("DOMContentLoaded", (e) => {
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        let avion = 20;
-        let proyect = 10;
-        let distancia = 0;
+        let cont = 1;
+        let gTotal = 0;
+        let canVendedores = document.getElementById("num1").value;
 
-        while(distancia < 1000){
-            avion = avion * 2;
-            proyect = proyect * 2;
-            distancia = avion + proyect;
+        while(cont <= canVendedores){
+            let sb = parseInt(prompt('vendedor #'+cont+'\ningrese su sueldo base'));
+            let v1 = parseInt(prompt('ingrese el valor de su primera venta:'));
+            let v2 = parseInt(prompt('ingrese el valor de su segunda venta:'));
+            let v3 = parseInt(prompt('ingrese el valor de su tercera venta:'));
+
+            let comision = (v1 + v2 + v3) * 0.10;
+            gTotal = sb + comision;
+            cont ++;
+
+            document.getElementById('res1').innerHTML='ganancias por comision: '+ comision+ ' pesos';
+            document.getElementById('res2').innerHTML='ganancias totales: '+ gTotal+ ' pesos';
         }
-        document.getElementById('res1').innerHTML= distancia +' m';
+        
 
 
     })
